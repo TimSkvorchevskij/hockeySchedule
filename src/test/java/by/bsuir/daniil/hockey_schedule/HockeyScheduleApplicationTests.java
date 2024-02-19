a28371cb19cfef7d75340b577602b06a845718dc
Daniil Voropaev <vorop2004@mail.ru>
2024-02-19 22:32:03 +0300

    Labs1Ready?

--- src/test/java/by/bsuir/daniil/hockey_schedule/HockeyScheduleApplicationTests.java
+++ src/test/java/by/bsuir/daniil/hockey_schedule/HockeyScheduleApplicationTests.java
@@ -1,13 +1,21 @@
 package by.bsuir.daniil.hockey_schedule;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class HockeyScheduleApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
